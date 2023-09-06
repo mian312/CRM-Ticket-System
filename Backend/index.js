c94db82ff1eed71a28a7530763946933f094fdb1
Milan <deymilan312@gmail.com>
2023-09-06 12:44:28 +0530

    Setup backend api for CRM ticket system

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,47 @@
-console.log('hello world')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+//API security
+app.use(helmet());
+
+//handle CORS error
+app.use(cors());
+
+//Logger
+app.use(morgan("tiny"));
+
+// Set body bodyParser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+
+//Load routers
+const userRouter = require("./src/routers/user.router");
+const ticketRouter = require("./src/routers/ticket.router");
+
+//Use Routers
+app.use("/api/user", userRouter);
+app.use("/api/ticket", ticketRouter);
+
+//Error handler
+const handleError = require("./src/utils/errorHandler");
+
+app.use((req, res, next) => {
+  const error = new Error("Resources not found!");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  handleError(error, res);
+});
+
+app.listen(port, () => {
+  console.log(`API is ready on http://localhost:${port}`);
+});
