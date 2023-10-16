49f31d86fbfaf50a4cacaf10b09765e85c0c59ac
Milan <deymilan312@gmail.com>
2023-10-16 20:56:55 +0530

    Backend: Get all tickets api added

--- Backend/src/model/ticket/Ticket.model.js
+++ Backend/src/model/ticket/Ticket.model.js
@@ -1,19 +1,39 @@
 import { TicketSchema } from "./Ticket.schema.js";
 
-// Define function to insert a new ticket
+//* Define function to insert a new ticket
 const insertTicket = (ticketObj) => {
-  // Return a Promise that handles the ticket insertion
-  return new Promise((resolve, reject) => {
-    try {
-      // Create a new instance of the TicketSchema and save it
-      new TicketSchema(ticketObj)
-        .save()
-        .then((data) => resolve(data)) // If the save is successful, resolve with the saved data
-        .catch((error) => reject(error)); // If there's an error during save, reject with the error
-    } catch (error) {
-      reject(error); // Catch and reject any other errors
-    }
-  });
+    // Return a Promise that handles the ticket insertion
+    return new Promise((resolve, reject) => {
+        try {
+            // Create a new instance of the TicketSchema and save it
+            new TicketSchema(ticketObj)
+                .save()
+                .then((data) => resolve(data)) // If the save is successful, resolve with the saved data
+                .catch((error) => reject(error)); // If there's an error during save, reject with the error
+        } catch (error) {
+            reject(error); // Catch and reject any other errors
+        }
+    });
 };
 
-export default insertTicket;
+
+//* Define function to get all tickets
+const getTickets = (clientId) => {
+    // Return a Promise that handles the ticket retrieval
+    return new Promise((resolve, reject) => {
+        try {
+            // Create a new instance of the TicketSchema and save it
+            TicketSchema.find({ clientId })
+                .then((data) => resolve(data))  // If search is successful, resolve with the found data
+                .catch((error) => reject(error));   // If there's an error during search, reject with the error
+        
+        } catch (error) {
+            reject(error);  // Catch and reject any other errors
+        }
+    });
+};
+
+export {
+    insertTicket,
+    getTickets
+};
--- Backend/src/routers/ticket.router.js
+++ Backend/src/routers/ticket.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import insertTicket from "../model/ticket/Ticket.model.js";
+import { getTickets, insertTicket } from "../model/ticket/Ticket.model.js";
+import { userAuthorization } from "../middleware/authorization.middleware.js";
 const router = Router();
 
 router.all("/", (req, res, next) => {
@@ -42,6 +43,26 @@ router.post("/", async (req, res) => {
     res.json({
       status: "error",
       message: "Unable to create the ticket, please try again later",
+    });
+  } catch (error) {
+    // If an error occurs, return an error response with the error message
+    res.json({ status: "error", message: error.message });
+  }
+});
+
+//* Define router for getting all tickets
+router.get("/", userAuthorization, async (req, res) => {
+  try {
+    // Retrieve the user's ID from the request object
+    const userId = req.userId;
+
+    // Call the "getTickets" function to retrieve tickets associated with the user
+    const result = await getTickets(userId);
+
+    // If the operation is successful, return a JSON response with the tickets
+    return res.json({
+      status: "success",
+      result,
     });
   } catch (error) {
     // If an error occurs, return an error response with the error message
